311a0ff3ee01e5be44e29377f6d87063694e9021
zhangshixin <zhangshixin@yaomaiche.com>
2017-08-22 00:09:53 +0800

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Tomato Time 
+
+MVP + Dagger2
+
+## Thanks
+
+这个项目基于 google sample: [android-architecture](https://github.com/googlesamples/android-architecture) 的 ``todo-mvp-dagger``分支，在它的基础上增加了一些特性和贡呢。
+
+
+再次感谢项目贡献者 [Saúl Molinero](https://github.com/saulmm) ！
+
+## 
---
+++ app/src/mock/java/com/example/android/architecture/blueprints/todoapp/data/source/TasksRepositoryModule.java
@@ -0,0 +1,26 @@
+package com.example.android.architecture.blueprints.todoapp.data.source;
+
+import com.example.android.architecture.blueprints.todoapp.data.FakeTasksRemoteDataSource;
+import com.example.android.architecture.blueprints.todoapp.data.source.local.TasksLocalDataSource;
+
+import javax.inject.Singleton;
+
+import dagger.Binds;
+import dagger.Module;
+
+/**
+ * This is used by Dagger to inject the required arguments into the {@link TasksRepository}.
+ */
+@Module
+abstract class TasksRepositoryModule {
+
+    @Singleton
+    @Binds
+    @Local
+    abstract TasksDataSource provideTasksLocalDataSource(TasksLocalDataSource dataSource);
+
+    @Singleton
+    @Binds
+    @Remote
+    abstract TasksDataSource provideTasksRemoteDataSource(FakeTasksRemoteDataSource dataSource);
+}
